9c414ca00f864d3e408948b50c4e2fdd32b8d841
jung_jung <kiki128qk@gmail.com>
2021-01-22 15:01:51 +0900

    1/22 글쓰기 게시판 UI 제작 중

--- gold/pages/qna/qna.php
+++ gold/pages/qna/qna.php
@@ -46,15 +46,66 @@
             <div class="qnaBoxes deWeBoxes">
                 <div class="qnaTable">
                     <ul class="qnaList">
-                        <li class="qnaTitle">
+                    <li class="qnaTitle clear">
                             <span class="qnaNum">번호</span>
                             <span class="qnaId">아이디</span>
                             <span class="qnaTit">제목</span>
                             <span class="qnaReg">등록일</span>
                             <span class="qnaHit">조회수</span>
                         </li>
+                        <li class="qnaContent clear">
+                            <span class="qnaNum">1</span>
+                            <span class="qnaId">test</span>
+                            <span class="qnaTit"><a href="#"> 첫 개시글 입니다.</a></span>
+                            <span class="qnaReg">2021-01-22</span>
+                            <span class="qnaHit">99</span>
+                        </li>
+                        <li class="qnaContent clear">
+                            <span class="qnaNum">2</span>
+                            <span class="qnaId">test1</span>
+                            <span class="qnaTit"><a href="#"> 두번째 개시글 입니다.</a></span>
+                            <span class="qnaReg">2021-01-22</span>
+                            <span class="qnaHit">12</span>
+                        </li>
+                        <li class="qnaContent clear">
+                            <span class="qnaNum">3</span>
+                            <span class="qnaId">test2</span>
+                            <span class="qnaTit"><a href="#"> 세번째 개시글 입니다.</a></span>
+                            <span class="qnaReg">2021-01-22</span>
+                            <span class="qnaHit">12</span>
+                        </li>
                     </ul>
                 </div>
+                <!-- end of qna table -->
+                <div class="searchPaging clear">
+                    <div class="search">
+                        <form action="abc.php" name="qnaSearch"  class="clear">
+                            <select name="searchSelact" id=""class="searchSelact">
+                                <option value="qnaSearchTitle">제목</option>
+                                <option value="qnaSearchId">아이디</option>
+                            </select>
+                            <input type="text" name="qnaSearchInput" class="qnaSearchInput" placeholder="검색어를 입력해주세요.">
+                            <button type="button" class="qnaSrarchBtn"><i class="fa fa-search"></i></button>
+                        </form>
+
+                    </div>
+                    <div class="paging">
+                        <span class="firstPg"><i class="fa fa-angle-double-left"></i></span>
+                        <span class="prevPg"><i class="fa fa-angle-left"></i></span>
+                        <span class="PgNum active">1</span>
+                        <span class="PgNum">2</span>
+                        <span class="prevPg"><i class="fa fa-angle-right"></i></span>
+                        <span class="lastPg"><i class="fa fa-angle-double-right"></i></span>
+                    </div>                
+                </div>
+                <!-- end of search paging -->
+                <div class="WriteBox">
+                        <form action="abc.php" method="post">
+                            <input type="text" placeholder="질문을 작성해 주세요.">
+                            <input type="text" placeholder="질문을 작성해 주세요.">
+                            <button class="writeBtn"> 글쓰기</button>                  
+                        </form>
+                    </div>
             </div>
         </div>
         <!-- end of center -->
